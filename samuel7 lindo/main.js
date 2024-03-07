95aa36e0c2e5028472788b46aaa95f582abe2551
samuel7qy <110116396+samuel7qy@users.noreply.github.com>
2024-03-07 08:58:30 -0300

    Add files via upload

--- samuel7 lindo/main.js
+++ samuel7 lindo/main.js
@@ -6,9 +6,11 @@ for(let i=0;i <botoes.length;i++){
       
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo")
+            textos[j].classList.remove("ativo")
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo")
     }
     console.log();
 }
